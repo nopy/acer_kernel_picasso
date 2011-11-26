17658365ad6dcb510f758572dbbb1dabce6eeae4
Nicolas Pinot <npinot@gmail.com>
2011-11-26 12:32:32 +0100

    Add Android.mk to build kernel within AOSP build system

---
+++ Android.mk
@@ -0,0 +1,26 @@
+#Android makefile to build kernel as a part of Android Build
+#Its here and not under gaosp-kernel/ for compatiblity with the froyo branch
+#The config for the kernel is arch/arm/configs/gaosp_msm_defconfig
+LOCAL_PATH := $(call my-dir)
+
+ifdef BUILD_KERNEL
+ifeq ($(TARGET_BOOTLOADER_BOARD_NAME),picasso)
+KERNEL_OUT := $(TARGET_OUT_INTERMEDIATES)/kernel
+KERNEL_CONFIG := $(KERNEL_OUT)/.config
+TARGET_PREBUILT_KERNEL := $(KERNEL_OUT)/arch/arm/boot/zImage
+$(TARGET_PREBUILT_KERNEL):= build_kernel
+
+$(INSTALLED_KERNEL_TARGET): $(TARGET_PREBUILT_KERNEL) | $(ACP)
+	$(transform-prebuilt-to-target)
+
+$(KERNEL_OUT):
+	mkdir -p $(KERNEL_OUT)
+
+$(KERNEL_CONFIG): $(LOCAL_PATH)/arch/arm/configs/picasso_defconfig | $(ACP)
+	$(transform-prebuilt-to-target)
+
+$(TARGET_PREBUILT_KERNEL): $(KERNEL_OUT) $(KERNEL_CONFIG)
+	$(MAKE) -C kernel-picasso O=../$(KERNEL_OUT) ARCH=arm CROSS_COMPILE=$(shell pwd)/prebuilt/linux-x86/toolchain/arm-eabi-4.4.0/bin/arm-eabi- INSTALL_MOD_STRIP=1
+endif
+endif
+
